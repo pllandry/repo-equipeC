a9ede7c706036fbf897d58beae551126bb0c55a8
defoyphilip <philipdefoy@gmail.com>
2015-06-16 20:14:24 -0400

    Added a flufin missing jar

--- src/main/java/com/gti525/socket/helper/Inscription.java
+++ src/main/java/com/gti525/socket/helper/Inscription.java
@@ -20,7 +20,7 @@ public class Inscription {
 	      String host = "localhost";
 	      Properties properties = System.getProperties();
 	      properties.setProperty("mail.smtp.host", host);
-	      Session session = Session.getDefaultInstance(properties);
+	      Session session = Session.getInstance(properties);
 	      try{
 	         MimeMessage message = new MimeMessage(session);
 	         message.setFrom(new InternetAddress(from));
